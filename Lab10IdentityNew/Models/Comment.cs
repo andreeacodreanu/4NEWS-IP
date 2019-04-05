157c3186c25005d3872584f5f91ba213c2550771
Florea Andrada <33199270+floreaandrada@users.noreply.github.com>
2019-04-05 13:44:45 +0300

    Add files via upload

---
+++ Lab10IdentityNew/Models/Comment.cs
@@ -0,0 +1,30 @@
+﻿using ProiectIP2019.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace ProiectIP.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int CommentId { get; set; }
+        [Required]
+        public string CommentContent { get; set; }
+        public DateTime CommentDate { get; set; }
+
+        public string UserId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+
+        public int ArticleId { get; set; }
+        public virtual ApplicationUser Article { get; set; }
+
+
+
+    }
+}
