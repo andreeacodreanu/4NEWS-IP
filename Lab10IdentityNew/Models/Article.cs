aa13042203830ceef290dd5a087056b159600fb6
Florea Andrada <33199270+floreaandrada@users.noreply.github.com>
2019-04-05 13:43:57 +0300

    Add files via upload

---
+++ Lab10IdentityNew/Models/Article.cs
@@ -0,0 +1,57 @@
+﻿using ProiectIP2019.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+
+namespace ProiectIP.Models
+{
+    public class Article
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+        public DateTime Date { get; set; }
+
+
+        //[RegularExpression(".(jpg|png|gif)", ErrorMessage = "Please use an image with an extension of .jpg, .png, .gif, .bmp")]
+        [Required(ErrorMessage = "Image is required")]
+        //[RegularExpression(@".+(jpg|png|gif)$", ErrorMessage = "Please use an image with an extension of .jpg, .png, .jpeg")]       
+        public string Image { get; set; }
+        [RegularExpression(@"^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$", ErrorMessage = "Invalid URL")]
+        public string Link { get; set; }
+
+
+        public string Approved { get; set; }
+        public int NrComm { get; set; }
+
+        public string Resume { get; set; }
+
+        public virtual ICollection<Comment> Comment { get; set; }
+
+       
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+
+
+        public string UserId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+
+
+        // Se aduaga acest atribut pentru a putea prelua toate categoriile unui model in helper
+        
+        public IEnumerable<SelectListItem> Categories { get; set; }
+
+    }
+
+    
+}
---
+++ Lab10IdentityNew/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProiectIP.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        [Required(ErrorMessage = "Category is required")]
+        public string CategoryName { get; set; }
+
+        public virtual ICollection<Article> Article { get; set; }
+
+    }
+}
